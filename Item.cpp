af55a332adb33f0a8f6655215013956604d322ee
Julian Knudson <julianknudson@Julians-MacBook-Pro.local>
2021-10-13 17:20:23 -0400

    item updates

--- Item.cpp
+++ Item.cpp
@@ -2,24 +2,26 @@
 #include "Thing.h"
 
 Item::Item() : Thing () {
-    std::cout << "Item constructor with name " << name << std::endl;
+    std::cout << "Creating an Item" << std::endl;
 }
 
 void Item::setDescription(std::string descToSet) {
-    std::cout << "Set Item description to " << descToSet << std::endl;
+    std::cout << "Setting item description to " << descToSet << std::endl;
     desc = descToSet;
 }
 
 void Item::setWriting(std::string writingToSet) {
-    std::cout << "Set Item writing to " << writingToSet << std::endl;
+    std::cout << "Setting item writing to " << writingToSet << std::endl;
     writing = writingToSet;
 }
 
 void Item::setTurnonAction(std::string turnonActionToSet) {
+    std::cout << "Setting the turnon action to " << turnonActionToSet << std::endl;
     turnonAction = turnonActionToSet;
 }
 
 void Item::setTurnonPrint(std::string turnonPrintToSet) {
+    std::cout << "Setting the turnon print to " << turnonPrintToSet << std::endl;
     turnonPrint = turnonPrintToSet;
 }
 
