7720806e34c343706168c29bb0e7d5787733249a
lritchis <lritchis@purdue.edu>
2021-10-13 02:44:19 -0400

    Fix Creature and Container classes2

--- Creature.cpp
+++ Creature.cpp
@@ -5,5 +5,6 @@ Creature::Creature() : Thing() {
 }
 
 void Creature::addVulnerability(std::string vulnToAdd) {
+    std::cout << "Added vulterability of " << vulnToAdd << std::endl;
     vulnerability.push_back(vulnToAdd);
 }
